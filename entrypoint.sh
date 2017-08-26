945c92468a71fed3b4c9756a7144d78382c6af65
Roliga <roliga.here@gmail.com>
2017-08-27 01:42:27 +0200

    Initial import

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Kill the current process group on relevant signals, unbinding the TERM signal to prevent recursion
+trap "trap - TERM && kill 0" INT EXIT TERM
+
+# Start services in the background, and kill this process group if one of them exits
+{ deluge-web -c /etc/deluge/; kill 0; } &
+{ deluged -d -c /etc/deluge/; kill 0; } &
+
+# Wait for all processes to exit, allowing traps to be handled
+wait
